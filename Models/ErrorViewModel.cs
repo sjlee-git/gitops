5c5082205cdba7e7e14d1b0b5d5c06f696eb7584
sjlee-git <sjlee-git@github.com>
2021-10-22 16:33:41 +0900

    test

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gitops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
